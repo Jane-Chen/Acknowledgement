b35ff447fac0a45dc0a80da610423d4c93159c86
Jane Chen <finnbubble@hotmail.com>
2013-09-08 22:09:32 +0800

    The first commition of acknowledgement.

---
+++ acknowledgement.py
@@ -0,0 +1,7 @@
+# This is an acknowledgement. Surely it is also just a test for the use of GitHub.
+
+def Acknowledgement():
+	print "Thanks for all the helps from cedarwu."
+
+if __name__ == '__main__':
+	Acknowledgement()
